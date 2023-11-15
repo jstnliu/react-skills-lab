169444c1e7dbd8144c94a225a269175e65b3dcb4
jstnliu <jlcheeseburger@gmail.com>
2023-11-15 14:54:09 -0800

    part 3 complete, updated w/initialSkills array

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,28 @@ import './App.css'
 import SkillList from './SkillList'
 // import new Skill Form component
 import NewSkillForm from './NewSkillForm'
+import { useState } from 'react'
+
+const initialSkills = [
+  {name: 'Mobility', level: 40},
+  {name: 'Resilience', level: 60 },
+  {name: 'Recovery', level: 100},
+  {name: 'Discipline', level: 70},
+  {name: 'Intellect', level: 40},
+  {name: 'Strength', level: 50},
+]  
 
 function App() {
+
+  const [skills, setSkills] = useState(initialSkills)
 
   return (
     <div className='App thistle-text'>
      <h1>Dev Skills</h1>
-     <SkillList />
+     <SkillList skills={skills} />
      <hr></hr>
      {/* Add NewSkillForm component */}
-     <NewSkillForm />
+     <NewSkillForm skills={skills} />
     </div>
   )
 }
--- src/SkillList.jsx
+++ src/SkillList.jsx
@@ -2,16 +2,7 @@
 import SkillListItem from './SkillListItem'
 import './SkillList.css'
 
-const skills = [
-    {name: 'Mobility', level: 40},
-    {name: 'Resilience', level: 60 },
-    {name: 'Recovery', level: 100},
-    {name: 'Discipline', level: 70},
-    {name: 'Intellect', level: 40},
-    {name: 'Strength', level: 50},
-  ]  
-
-export default function SkillList() {
+export default function SkillList({ skills }) {
     
     const skillListItems = skills.map((skill, idx) => <SkillListItem skill={skill} index={idx} key={idx} />)
     
